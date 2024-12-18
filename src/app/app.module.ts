1e721d2c2dfb03a8e9a8482450c1ee7a2c266b86
Isaque <isaquebatista400@gmail.com>
2024-12-18 17:17:12 -0300

    :sparkles: Feat: Component Calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -10,6 +10,10 @@ import { HomeModule } from './modules/home/home.module';
 import { MaintenanceModule } from './modules/maintenance/maintenance.module';
 import { ScheduleModule } from './modules/schedule/schedule.module';
 
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt, 'pt');
 @NgModule({
   declarations: [
     AppComponent
@@ -24,7 +28,9 @@ import { ScheduleModule } from './modules/schedule/schedule.module';
     HttpClientModule,
     ScheduleModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pt'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/modules/schedule/pages/create-appointment-page/create-appointment-page.component.ts
+++ src/app/modules/schedule/pages/create-appointment-page/create-appointment-page.component.ts
@@ -7,7 +7,8 @@ import { AppointmentType } from '../../../../core/models/appointmentType';
 import { ClientService } from '../../../../core/services/client.service';
 import { debounceTime, distinctUntilChanged, filter, Observable, switchMap } from 'rxjs';
 import { Client } from '../../../../core/models/client';
-import { FormCreateAppointmentComponent } from '../../component/form-create-appointment/form-create-appointment.component';
+import { FormCreateAppointmentComponent } from '../../components/form-create-appointment/form-create-appointment.component';
+import { ProfessionalService } from '../../../../core/services/professional.service';
 
 @Component({
   selector: 'app-create-appointment-page',
@@ -18,18 +19,42 @@ export class CreateAppointmentPageComponent implements OnInit{
   areas: Area[] = []
   professionalsByArea: Professional[] = []
   appointmentTypes: AppointmentType[] = []
+  selectedProfessional: Professional = {} as Professional
+
+  calendarMonth: Date = new Date()
+  avaiableDays: number[] = []
 
   @ViewChild(FormCreateAppointmentComponent)
   private formCreateAppointComponent !: FormCreateAppointmentComponent
 
   constructor(private areaService: AreaService,
               private appointmentTypeService: AppointmentTypeService,
-              private clientService: ClientService
+              private clientService: ClientService,
+              private professionalService: ProfessionalService
             ){}
 
   ngOnInit(): void {
     this.loadAreas()
     this.loadAppointmentTypes()
+  }
+
+  onSelectedDate(date: Date){}
+
+  onChangedMonth(date: Date){
+    this.calendarMonth = date
+    this.loadAvaiblesDays()
+  }
+
+  onSelectedProfessional(professional: Professional){
+    this.selectedProfessional = professional,
+    this.calendarMonth = new Date()
+    this.loadAvaiblesDays()
+  }
+
+  loadAvaiblesDays(){
+    this.professionalService.getAvailableDays(this.selectedProfessional, this.calendarMonth).subscribe({
+      next: avaibleDays => this.avaiableDays = avaibleDays,
+    })
   }
 
   searchClients = (text: Observable<string>): Observable<Client[]> => {
@@ -63,7 +88,7 @@ export class CreateAppointmentPageComponent implements OnInit{
   }
 
   createAppointment(){
-    this.formCreateAppointComponent.submitted = true  
+    this.formCreateAppointComponent.submitted = true
     console.info(this.formCreateAppointComponent.appointmentForm.value)
   }
 }
